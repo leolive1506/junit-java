74f74c8d93ccc77c4ebf7408d0e9ea27ebf23413
Leonardo Lopes <leonardo.santana@devsquad.com>
2024-06-20 21:49:49 -0300

    symple test

---
+++ src/test/java/org/example/domain/PessoaTest.java
@@ -0,0 +1,24 @@
+package org.example.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class PessoaTest {
+    @Test
+    void deveCalcularIdadeCorretamente() {
+        var pessoa = new Pessoa("Leonardo", LocalDateTime.of(2004, 6, 25, 00, 00));
+        Assertions.assertEquals(19, pessoa.getIdade());
+    }
+
+    @Test
+    void deveRetornarSeMaiorIdade() {
+        var pessoa = new Pessoa("Leonardo", LocalDateTime.of(2004, 6, 25, 00, 00));
+        Assertions.assertTrue(pessoa.maiorDeIdade());
+
+        pessoa = new Pessoa("Joao", LocalDateTime.now());
+        Assertions.assertFalse(pessoa.maiorDeIdade());
+    }
+
+}

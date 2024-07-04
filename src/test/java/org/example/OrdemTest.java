159121399930ffe21f0df77984195a1733803cbc
Leonardo Lopes <leonardo.santana@devsquad.com>
2024-07-04 19:26:34 -0300

    order test by method name

--- src/test/java/org/example/OrdemTest.java
+++ src/test/java/org/example/OrdemTest.java
@@ -2,28 +2,24 @@ package org.example;
 
 import org.junit.jupiter.api.*;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class OrdemTest {
     @Test
-    @Order(4)
     void validaFluxoA() {
         Assertions.assertTrue(true);
     }
 
     @Test
-    @Order(3)
     void validaFluxoB() {
         Assertions.assertTrue(true);
     }
 
     @Test
-    @Order(2)
     void validaFluxoC() {
         Assertions.assertTrue(true);
     }
 
     @Test
-    @Order(1)
     void validaFluxoD() {
         Assertions.assertTrue(true);
     }

10805c282dbf791fd18eefac574c09773a21b79d
Leonardo Lopes <leonardo.santana@devsquad.com>
2024-07-04 19:28:37 -0300

    order by display name

--- src/test/java/org/example/OrdemTest.java
+++ src/test/java/org/example/OrdemTest.java
@@ -2,24 +2,28 @@ package org.example;
 
 import org.junit.jupiter.api.*;
 
-@TestMethodOrder(MethodOrderer.Random.class)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
 public class OrdemTest {
     @Test
+    @DisplayName("B")
     void validaFluxoA() {
         Assertions.assertTrue(true);
     }
 
     @Test
+    @DisplayName("A")
     void validaFluxoB() {
         Assertions.assertTrue(true);
     }
 
     @Test
+    @DisplayName("C")
     void validaFluxoC() {
         Assertions.assertTrue(true);
     }
 
     @Test
+    @DisplayName("D")
     void validaFluxoD() {
         Assertions.assertTrue(true);
     }
